a6ecf637bc36e9d9f6a931544391b61905effc37
viyer14 <64116086+viyer14@users.noreply.github.com>
2022-12-05 04:10:17 +0530

    Cars base

---
+++ resources/resources/[start]/[vehicles]/enginev10/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/dlcenginev10_game.dat151.rel',
+  'audioconfig/dlcenginev10_sounds.dat54.rel',
+  'sfx/dlc_enginev10/enginev10.awc',
+  'sfx/dlc_enginev10/enginev10_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/dlcenginev10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/dlcenginev10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_enginev10'
